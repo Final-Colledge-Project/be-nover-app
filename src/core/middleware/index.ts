5eb350df91f8fccc5366fe955018985bdaf60ab4
khiemld <khiemit240@gmail.com>
2023-09-24 22:20:44 +0700

    [build]: build infrastructure, install middleware and config module paths

---
+++ src/core/middleware/index.ts
@@ -0,0 +1,4 @@
+import errorMiddleWare from "./error.middleware";
+
+
+export {errorMiddleWare} 
---
+++ src/modules/index/index.controller.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export default class IndexController {
+  public index = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(201).json({
+        "Message": "Hello word"
+      })
+    }
+    catch (err){
+      next()
+    }
+  }
+}
