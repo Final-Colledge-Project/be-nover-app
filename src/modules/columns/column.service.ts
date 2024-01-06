17e760692757431fd518a9cd348d3e5a159f29f8
Khiem Le <khiem.le@devsamurai.com>
2024-01-06 15:30:53 +0700

    bugs/BE-038: Fix move card permission

--- src/modules/columns/column.service.ts
+++ src/modules/columns/column.service.ts
@@ -29,8 +29,11 @@ export default class ColumnService {
       throw new HttpException(StatusCodes.CONFLICT, "Board not found");
     }
     const checkPermissionCol = await permissionColumn(board.id, userId);
-    if(!checkPermissionCol){
-      throw new HttpException(StatusCodes.FORBIDDEN, "You have not permission to create column");
+    if (!checkPermissionCol) {
+      throw new HttpException(
+        StatusCodes.FORBIDDEN,
+        "You have not permission to create column"
+      );
     }
     const existColumn = await this.columnSchema.findOne({
       title: model.title,
@@ -65,8 +68,11 @@ export default class ColumnService {
     boardId: string,
     userId: string
   ): Promise<IColumn[]> {
-    if (await viewedBoardPermission(boardId, userId) === false) {
-      throw new HttpException(StatusCodes.FORBIDDEN, "You are not member of this board");
+    if ((await viewedBoardPermission(boardId, userId)) === false) {
+      throw new HttpException(
+        StatusCodes.FORBIDDEN,
+        "You are not member of this board"
+      );
     }
     const columns = await this.columnSchema
       .find({ boardId })
@@ -77,47 +83,79 @@ export default class ColumnService {
     }
     return columns;
   }
-  public async updateColumn(model: UpdateColumnDto, columnId: string, userId: string) : Promise<IColumn> {
-    if(isEmptyObject(model)){
+  public async updateColumn(
+    model: UpdateColumnDto,
+    columnId: string,
+    userId: string
+  ): Promise<IColumn> {
+    if (isEmptyObject(model)) {
       throw new HttpException(StatusCodes.BAD_REQUEST, "Model is empty");
     }
     const existColumn = await this.columnSchema.findById(columnId).exec();
-    if(!existColumn){
+    if (!existColumn) {
       throw new HttpException(StatusCodes.CONFLICT, "Column not found");
     }
-    const checkPermissionCol = await permissionColumn(existColumn.boardId, userId);
-    if(!checkPermissionCol){
-      throw new HttpException(StatusCodes.FORBIDDEN, "You have not permission to update column");
-    }
-    if(model.title) {
-      const existTitle = await this.columnSchema.findOne({title: model.title, boardId: existColumn.boardId}).exec();
-      if(existTitle){
-        throw new HttpException(StatusCodes.CONFLICT, `Column with title ${model.title} already exists`);
+    if (!model.cardOrderIds) {
+      const checkPermissionCol = await permissionColumn(
+        existColumn.boardId,
+        userId
+      );
+      if (!checkPermissionCol) {
+        throw new HttpException(
+          StatusCodes.FORBIDDEN,
+          "You have not permission to update column"
+        );
       }
     }
-    const updatedColumn = await this.columnSchema.findByIdAndUpdate(columnId, {
-      ...model,
-      updatedAt: Date.now(),
-    }, {new: true}).exec();
-    if(!updatedColumn){
+
+    if (model.title) {
+      const existTitle = await this.columnSchema
+        .findOne({ title: model.title, boardId: existColumn.boardId })
+        .exec();
+      if (existTitle) {
+        throw new HttpException(
+          StatusCodes.CONFLICT,
+          `Column with title ${model.title} already exists`
+        );
+      }
+    }
+    const updatedColumn = await this.columnSchema
+      .findByIdAndUpdate(
+        columnId,
+        {
+          ...model,
+          updatedAt: Date.now(),
+        },
+        { new: true }
+      )
+      .exec();
+    if (!updatedColumn) {
       throw new HttpException(StatusCodes.CONFLICT, "Column not updated");
     }
     return updatedColumn;
   }
-  public async deleteColumn(columnId: string, userId: string) : Promise<IColumn> {
+  public async deleteColumn(
+    columnId: string,
+    userId: string
+  ): Promise<IColumn> {
     const column = await this.columnSchema.findById(columnId).exec();
-    if(!column){
+    if (!column) {
       throw new HttpException(StatusCodes.CONFLICT, "Column not found");
     }
     const checkPermissionCol = await permissionColumn(column.boardId, userId);
-    if(!checkPermissionCol){
-      throw new HttpException(StatusCodes.FORBIDDEN, "You have not permission to delete column");
+    if (!checkPermissionCol) {
+      throw new HttpException(
+        StatusCodes.FORBIDDEN,
+        "You have not permission to delete column"
+      );
     }
-    if(column.cardOrderIds.length > 0){
+    if (column.cardOrderIds.length > 0) {
       throw new HttpException(StatusCodes.CONFLICT, "Column not empty");
     }
-    const deletedColumn = await this.columnSchema.findByIdAndDelete(columnId).exec();
-    if(!deletedColumn){
+    const deletedColumn = await this.columnSchema
+      .findByIdAndDelete(columnId)
+      .exec();
+    if (!deletedColumn) {
       throw new HttpException(StatusCodes.CONFLICT, "Column not deleted");
     }
     await BoardSchema.findByIdAndUpdate(
