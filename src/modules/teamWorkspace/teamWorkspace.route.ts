4e0eb20ba7d9ca39a3b1dc040f4197b727c31848
khiemld <khiemld.0204@gmail.com>
2024-03-13 01:40:41 +0700

    feature/nov-3: Delete workspace

--- src/modules/teamWorkspace/teamWorkspace.route.ts
+++ src/modules/teamWorkspace/teamWorkspace.route.ts
@@ -1,40 +1,44 @@
-import { Router } from "express"
+import { Router } from "express";
 import TeamWorkspaceController from "./teamWorkspace.controller";
 import { Route } from "@core/interfaces";
 import { authMiddleware, validationMiddleware } from "@core/middleware";
 import CreateTeamWorkspaceDto from "./dtos/createTeamWorkspace.dto";
 import JoinGroupDto from "./dtos/joinGroup.dto";
 export default class TeamWorkspaceRoute implements Route {
-  public path = '/api/v1/team-workspace'
-  public router = Router()
-  public teamWorkspaceController = new TeamWorkspaceController()
-  constructor(){
+  public path = "/api/v1/team-workspace";
+  public router = Router();
+  public teamWorkspaceController = new TeamWorkspaceController();
+  constructor() {
     this.initializeRoute();
   }
 
-  private initializeRoute(){
+  private initializeRoute() {
     this.router.post(
       this.path,
-      validationMiddleware(CreateTeamWorkspaceDto, true), 
+      validationMiddleware(CreateTeamWorkspaceDto, true),
       authMiddleware,
       this.teamWorkspaceController.createTeamWorkspace
-    )
+    );
     this.router.post(
-      this.path + '/:id/assign-admin',
+      this.path + "/:id/assign-admin",
       validationMiddleware(JoinGroupDto, true),
       authMiddleware,
       this.teamWorkspaceController.assignMemberToAdmin
-    )
+    );
     this.router.get(
-      this.path + '/:id',
+      this.path + "/:id",
       authMiddleware,
       this.teamWorkspaceController.getTeamWorkspaceById
-    )
+    );
     this.router.get(
-      this.path + '/:id/members',
+      this.path + "/:id/members",
       authMiddleware,
       this.teamWorkspaceController.getMemberTeamWorkspace
-    )
+    );
+    this.router.delete(
+      this.path + "/:id",
+      authMiddleware,
+      this.teamWorkspaceController.deleteTeamWorkspace
+    );
   }
-  
 }
